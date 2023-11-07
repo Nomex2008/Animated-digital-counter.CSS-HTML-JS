3dad559c09237754cfd0b4bd8f532766c8c0867d
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-07 19:27:39 +0200

    Commit 2
    Add scroll animation.

--- js/mainJS.js
+++ js/mainJS.js
@@ -36,5 +36,33 @@ function windowLoad() {
         window.requestAnimationFrame(step);
     }
 
-    digitsCountersInit()
+    //digitsCountersInit()
+
+    //scroll animation
+    let options = {
+        threshold: .3,
+    }
+
+    let observer = new IntersectionObserver((entries,observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const targetElement = entry.target;
+                const digitsCountersItems = targetElement.querySelectorAll('[data-digits-counter]')
+    
+                if (digitsCountersItems.length) {
+                    digitsCountersInit(digitsCountersItems)
+                }
+    
+                //lock animayion s
+                //observer.unobserve(targetElement)
+            }
+        })
+    })
+
+    let sections = document.querySelectorAll('.page__section')
+    if (sections.length) {
+        sections.forEach(section => {
+            observer.observe(section)
+        })
+    }
 }
